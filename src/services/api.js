85955ac28c2df1ae11c85e90b6ab64ec97478d7f
meetybhuva1326 <meetupwork24@gmail.com>
2026-04-13 23:54:24 +0530

    change api base

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://task-manager-backend-0ejx.onrender.com/api/";
-const API_BASE_URL = "http://localhost:8081/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://task-manager-backend-0ejx.onrender.com/api/";
+// const API_BASE_URL = "http://localhost:8081/api";
 
 const API = axios.create({
     baseURL: API_BASE_URL,
